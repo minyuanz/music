05f6da53c209d6a7bc27a3736bc2b514d80bde3e
minyuan <a1097562@gmail.com>
2023-11-16 22:21:38 +0800

    'dist'

---
+++ dist/assets/Artist-103789b8.js
@@ -0,0 +1,1 @@
+import{_ as e,a as t,c as s,b as c}from"./index-bc2b7de7.js";const o={},n=c("h1",null,"artist",-1),r=[n];function a(_,i){return t(),s("div",null,r)}const d=e(o,[["render",a]]);export{d as default};
